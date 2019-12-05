0e2a0648d9f6e99c38864e2c409c74f853fe7c79
Christine Smith <christinesm@google.com>
2019-12-04 22:31:35 -0800

    mvp and one part of stretch

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const MovieCard = props => {
-  return;
+  return (
+    <div className="movie-card">
+      <h2>{props.title2}</h2>
+      <div className="movie-director">
+        Director: <em>{props.director2}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{props.metascore2}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {props.stars2.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieCard;
